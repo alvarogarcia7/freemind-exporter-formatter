bb3d0ad5b8d405d794d9addb84d9d5a1ae9fb00b
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-11-09 06:39:17 +0400

    Add typing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import xml.etree.ElementTree as xml
+from typing import Dict
 
 
 class MindMapFormatter:
@@ -7,14 +8,14 @@ class MindMapFormatter:
         self.path = statement_path
         self.program = formatter_name
 
-    def read(self):
+    def read(self) -> None:
         with open(self.path, "r") as file:
             tree = xml.parse(file)
             root = self._get_center_node(tree)
             self._print_tree(root)
 
     @staticmethod
-    def _get_center_node(tree):
+    def _get_center_node(tree: xml.ElementTree) -> xml.Element:
         """
         Skip the topmost node, a container for the head of the mindmap
         :param tree: the whole mindmap
@@ -22,7 +23,7 @@ class MindMapFormatter:
         """
         return tree.getroot()[0]
 
-    def _print_tree(self, root):
+    def _print_tree(self, root: xml.Element) -> None:
         module = __import__(self.program)
         module.Formatter().export(root)
 
--- mindmap_exporter.py
+++ mindmap_exporter.py
@@ -1,3 +1,6 @@
+import xml.etree.ElementTree as xml
+
+
 class MindmapExporter:
-    def export(self, tree):
+    def export(self, tree: xml.Element) -> None:
         raise NotImplementedError("Should have implemented this")
