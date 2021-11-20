d4b09077940c79e02c368fe9ea60e38eae537e8d
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-11-20 19:18:39 +0400

    [PPT] Add exporter for PPT

---
+++ ppt_slides.py
@@ -0,0 +1,36 @@
+from mindmap_exporter import MindmapExporter
+import xml.etree.ElementTree as xml
+
+
+class Formatter(MindmapExporter):
+    def export(self, tree: xml.Element) -> None:
+        self._print_tree(tree)
+
+    def _print_tree(self, root: xml.Element) -> None:
+        self._print_tree_as_titles(root, 0)
+
+    def _print_tree_as_titles(self, root: xml.Element, level: int) -> None:
+        node_text = root.attrib['TEXT']
+        if level == 1:
+            print(f"""
+C{node_text.upper()}
+""")
+        elif level == 2:
+            print(f"""
+{node_text.upper()}
+""")
+        elif level == 3:
+            print(f"{node_text}")
+
+        for child in root:
+            self._print_tree_as_titles(child, level + 1)
+
+        # if level == 1:
+        #     pass
+        # elif level == 2:
+        #     print("\\end{itemize}")
+        #     print("\\end{frame}")
+        # elif level == 3:
+        #     pass
+        # elif level == 4:
+        #     pass
