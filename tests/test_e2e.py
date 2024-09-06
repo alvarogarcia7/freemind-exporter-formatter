5c9fbc18c4093332be6ecd741e744a334213e402
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2024-09-06 17:47:50 +0400

    whitespace

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,11 +1,13 @@
 import unittest
 
-from tests.approval_tests.command_helper import CommandHelper
 from approvaltests import verify
+
+from tests.approval_tests.command_helper import CommandHelper
 
 
 class MyTestCase(unittest.TestCase):
     command_helper = CommandHelper()
+
     def test_leaf_as_text(self) -> None:
         verify(self.command_helper.invoke_command(self.command_helper.to_list("""\
 python3 main.py --input ./data/test1.mm --formatter leaf_as_text.py""")))
@@ -13,7 +15,6 @@ python3 main.py --input ./data/test1.mm --formatter leaf_as_text.py""")))
     def test_leaf_as_latex_slides(self) -> None:
         verify(self.command_helper.invoke_command(self.command_helper.to_list("""\
 python3 main.py --input ./data/test1.mm --formatter latex_slides.py""")))
-
 
     def test_titles(self) -> None:
         verify(self.command_helper.invoke_command(self.command_helper.to_list("""\
