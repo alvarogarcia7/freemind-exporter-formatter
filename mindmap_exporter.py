daf4cadd92772f6e74b4696b2dae954c0a81691c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2026-01-17 09:40:44 +0400

    design interface

--- mindmap_exporter.py
+++ mindmap_exporter.py
@@ -1,6 +1,19 @@
+import sys
 import xml.etree.ElementTree as xml
 
 
 class MindmapExporter:
+    def __init__(self) -> None:
+        self.out = sys.stdout
+        self.lines: list[str] = []
+
     def export(self, tree: xml.Element) -> None:
-        raise NotImplementedError("Should have implemented this")
+        self.parse(tree)
+        self.print()
+
+    def parse(self, tree: xml.Element) -> None:
+        raise NotImplementedError()
+
+    def print(self) -> None:
+        for line in self.lines:
+            print(line, file=self.out)
