8b92d5bba268503653b5de7ba0ad7391444e7768
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2026-01-17 09:40:44 +0400

    Refactor mindmap_orgmode_lists to use parse-then-print interface
    Adapt mindmap_orgmode_lists.py to the new MindmapExporter interface that
    separates parsing and formatting concerns:
    
    - Implement parse() method to traverse tree and build self.lines
    - Implement format() method to return formatted lines
    - Remove direct print() calls, use self.lines.append() instead
    - Update unit tests to call parse() and format() directly
    - All tests pass (169 total, no regressions)

--- mindmap_exporter.py
+++ mindmap_exporter.py
@@ -1,6 +1,41 @@
+import sys
 import xml.etree.ElementTree as xml
+from typing import Any
 
 
 class MindmapExporter:
+    def __init__(self) -> None:
+        self.out = sys.stdout
+        self.lines: list[str] = []
+        """
+        The parsed result of the XML tree
+        :type: Any - depends on the exporter
+        """
+        self.result: Any = None
+
     def export(self, tree: xml.Element) -> None:
-        raise NotImplementedError("Should have implemented this")
+        self.parse(tree)
+        self.lines = self.format()
+        self.print()
+
+    def parse(self, tree: xml.Element) -> None:
+        """
+        Parse the XML tree and store the result in `self.result`
+        :param tree:
+        """
+        raise NotImplementedError()
+
+    def print(self) -> None:
+        """
+        Print the formatted output lines to the output stream
+        The output content is printed line by line from the list `self.lines`
+        """
+        for line in self.lines:
+            print(line, file=self.out)
+
+    def format(self) -> list[str]:
+        """
+        Format the parsed result into a list of output lines
+        :return: list of formatted output lines
+        """
+        return self.lines.copy()
